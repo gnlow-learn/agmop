9cc42743c15379daf47c64b32f2fa396caf6a43a
Gnlow <gnlow@pm.me>
2025-08-13 16:10:53 +0900

    add dp

---
+++ src/dp.ts
@@ -0,0 +1,40 @@
+// @ts-types="https://esm.sh/@stdlib/random@0.3.3/base/beta/docs/types/index.d.ts"
+import beta from "https://esm.sh/@stdlib/random@0.3.3/base/beta/lib?standalone"
+
+// @ts-types="https://esm.sh/@stdlib/random@0.3.3/base/uniform/docs/types/index.d.ts"
+import uniform from "https://esm.sh/@stdlib/random@0.3.3/base/uniform/lib?standalone"
+
+import { PMF } from "./util.ts"
+
+const randSeed =
+() =>
+    Math.floor(
+        Math.random() * 2**16
+    )
+
+export const gem =
+function* (a: number, seed = randSeed()) {
+    let b = 1
+    const seededBeta = beta.factory({ seed })
+    while (1) {
+        const v = seededBeta(1, a)
+        yield v * b
+        b *= (1-v)
+    }
+}
+
+export const dp =
+<T>
+(
+    a: number,
+    h: (seed: number) => T,
+    sample = 10,
+    seed = randSeed(),
+) => {
+    const pmf = new PMF<T>()
+    const rand = uniform.factory({ seed })
+    gem(a, seed)
+        .take(sample)
+        .forEach(b => pmf.add(h(rand(0, 1)), b))
+    return pmf
+}
